3080bf8b2e64c6785246a6591a12147ad51289e0
Ryan <ryanccordeiro@gmail.com>
2022-04-12 21:28:55 -0300

    Dismissable e Confirm on delete

--- lib/src/list.dart
+++ lib/src/list.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_lista/model/contact.dart';
 import 'package:flutter_lista/service/contact_service.dart';
@@ -28,77 +30,116 @@ class _ListaWidgetState extends State<ListaWidget> {
           child: ListBody(
             children: contacts
                 .map((contact) => Card(
-                      child: ListTile(
-                        title: Text(contact.name),
-                        subtitle:
-                            Text("${contact.phoneNumber} - ${contact.email}"),
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(contact.image ??
-                              "https://picsum.photos/seed/${contact.name}/100"),
-                        ),
-                        onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return ContactFormPage(
-                              contact: contact,
+                      child: Dismissible(
+                        key: Key(contact.phoneNumber ?? ''),
+                        confirmDismiss: (direction) async {
+                          return await showModalBottomSheet(context: context, builder: (BuildContext context)  {
+                            return Container(
+                              color: Colors.red,
+                              child: ElevatedButton(
+                                child: Text('Apagar'),
+                                onPressed: (){
+                                  Navigator.pop(context,true);
+                                },
+                              ),
                             );
-                          })).then((value) {
-                            setState(() {
-                              contacts = contactService.getAll()!;
-                            });
                           });
                         },
-                        trailing: PopupMenuButton(
-                          itemBuilder: (context) => [
-                            PopupMenuItem(
-                              value: ContactAction.call,
-                              onTap: () async {
-                                launch("tel://${contact.phoneNumber}");
-                              },
-                              child: Row(
-                                children: [
-                                  Icon(Icons.call),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text("Ligar")
-                                ],
-                              ),
+                        onDismissed: (direction) {
+                          setState(() {
+                            contactService.deleteById(contact.id);
+                            contacts = contactService.getAll()!;
+                          });
+
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Contato ${contact.name} deletado') ));
+
+                        },
+                        background: Container(
+                          color: Colors.red,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Icon(Icons.delete),
+                                Icon(Icons.delete)
+                              ],
                             ),
-                            PopupMenuItem(
-                              value: ContactAction.sendEmail,
-                              onTap: () async {
-                                launch("mailto:${contact.email!}");
-                              },
-                              child: Row(
-                                children: [
-                                  Icon(Icons.mail),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text("Enviar Email")
-                                ],
+                          ),
+                          alignment: Alignment.centerRight,
+                        ) ,
+                        child: ListTile(
+                          title: Text(contact.name),
+                          subtitle:
+                              Text("${contact.phoneNumber} - ${contact.email}"),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(contact.image ??
+                                "https://picsum.photos/seed/${contact.name}/100"),
+                          ),
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return ContactFormPage(
+                                contact: contact,
+                              );
+                            })).then((value) {
+                              setState(() {
+                                contacts = contactService.getAll()!;
+                              });
+                            });
+                          },
+                          trailing: PopupMenuButton(
+                            itemBuilder: (context) => [
+                              PopupMenuItem(
+                                value: ContactAction.call,
+                                onTap: () async {
+                                  launch("tel://${contact.phoneNumber}");
+                                },
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.call),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text("Ligar")
+                                  ],
+                                ),
                               ),
-                            ),
-                            PopupMenuItem(
-                              value: ContactAction.delete,
-                              onTap: () {
-                                contactService.deleteById(contact.id);
-                                setState(() {
-                                  contacts = contactService.getAll()!;
-                                });
-                              },
-                              child: Row(
-                                children: [
-                                  Icon(Icons.delete),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text("Apagar")
-                                ],
+                              PopupMenuItem(
+                                value: ContactAction.sendEmail,
+                                onTap: () async {
+                                  launch("mailto:${contact.email!}");
+                                },
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.mail),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text("Enviar Email")
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                              PopupMenuItem(
+                                value: ContactAction.delete,
+                                onTap: () {
+                                  contactService.deleteById(contact.id);
+                                  setState(() {
+                                    contacts = contactService.getAll()!;
+                                  });
+                                },
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.delete),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text("Apagar")
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ))
